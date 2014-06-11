1264b1ab8c9c014d957ac205dc8808ffe93dfc48
JasonNgo97 <ngojason1997@gmail.com>
2014-06-10 22:12:13 -0700

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  Lab5.1
+//
+//  Created by Jason Ngo on 6/8/14.
+//  Copyright (c) 2014 Jason.INC. All rights reserved.
+//
+/*
+ 
+ Pointers and Dynamic memory allocation/de­allocation using new and delete Arrays of pointers
+ Elementary Sorting
+ Binary search
+ Friend functions Copy constructor
+ */
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
