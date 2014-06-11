b4c275926cb31fb5f34d9a30608f21e954fadef7
JasonNgo97 <ngojason1997@gmail.com>
2014-06-10 22:14:29 -0700

    Create FruitStation.cpp

---
+++ FruitStation.cpp
@@ -0,0 +1,32 @@
+//
+//  FruitStation.cpp
+//  Lab5.1
+//
+//  Created by Jason Ngo on 6/8/14.
+//  Copyright (c) 2014 Jason.INC. All rights reserved.
+//
+
+#include "FruitStation.h"
+
+
+
+FruitStation::FruitStation()
+{   /*
+     initialize all pointers to NULL
+     */
+    for (int i=0;i<Jason;i++)
+    {
+        obj[i]=0;
+        
+        
+    }
+}
+FruitStation::FruitStation(string fat)
+{
+    for (int x=0;x<Jason;x++)
+    {
+        
+    }
+    
+    
+}
