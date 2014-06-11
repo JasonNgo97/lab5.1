2f143c7aea6e654ab2dc01075558ebfd7cebea21
JasonNgo97 <ngojason1997@gmail.com>
2014-06-10 22:15:54 -0700

    Create Fruit.h

---
+++ Fruit.h
@@ -0,0 +1,21 @@
+//
+//  Fruit.h
+//  Lab5.1
+//
+//  Created by Jason Ngo on 6/8/14.
+//  Copyright (c) 2014 Jason.INC. All rights reserved.
+//
+
+#ifndef __Lab5_1__Fruit__
+#define __Lab5_1__Fruit__
+
+#include <iostream>
+class Fruit
+{
+    
+public:
+    
+    
+private:
+};
+#endif /* defined(__Lab5_1__Fruit__) */
